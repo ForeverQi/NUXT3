6ff3ad572a11c43b21b5340a4087c399d09f0b25
周琦 <794950889@qq.com>
2023-06-04 21:27:17 +0800

    feat: useState全局状态管理

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,23 @@
 <template>
   <h1>index.vue</h1>
+  <h2>{{ counter }}</h2>
+  <div>
+    <button @click="counter++">+</button>
+  </div>
   <NuxtLink to="/details">Details Page</NuxtLink>
 </template>
 <script setup>
 import api from '@/api/api';
 import { onMounted } from 'vue';
-  const params = {
-    raceId: '1000006100'
-  };
-  onMounted(async () => {
-    const response = await $fetch(api.eventItemDetailApiName,{query:{
+const params = {
+  raceId: '1000006100'
+};
+let counter = useCounter();
+onMounted(() => {
+  const response = $fetch(api.eventItemDetailApiName, {
+    query: {
       ...params
-    }});
-    console.log(response);
-  })
+    }
+  });
+});
 </script>
