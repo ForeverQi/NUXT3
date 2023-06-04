6ff0b41750f741cc713c686474b4f65ae027b84f
周琦 <794950889@qq.com>
2023-06-04 14:55:16 +0800

    删除.nuxt

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/zhouqi/Documents/important/NUXT/Nuxt3/NUXT3/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "/Users/zhouqi/Documents/important/NUXT/Nuxt3/NUXT3/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
